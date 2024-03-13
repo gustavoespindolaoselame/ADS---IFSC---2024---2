7e4c83f67a43d9e1670df9c3528027b04e7b34eb
gustavoespindolaoselame <gustavo.e.oselame@gmail.com>
2024-03-12 21:33:16 -0300

    git final

--- POO786202/Introduçao_ao_paradigma/app/src/main/java/introduçao_ao_paradigma/Batata.java
+++ POO786202/Introduçao_ao_paradigma/app/src/main/java/introduçao_ao_paradigma/Batata.java
@@ -1,33 +1,41 @@
 package introduçao_ao_paradigma;
 
+import java.util.Random;
+
 public class Batata {
     boolean capacete = false;
     boolean braco = false;
     int numasasabertas = 0;
     String[] falas = new String[6];
-    public Batata(){
+
+    public Batata() {
         falas[0] = "ao infinito e além";
         falas[1] = "sla 1";
         falas[2] = "oi";
         falas[3] = "aaaa";
         falas[4] = "bbbb";
         falas[5] = "ccccccccccccccc";
-    }   
-    public boolean retraircapacete(){
-        if(capacete==true){
-            capacete = false;
-            System.out.println("capacete abaixado");
-        } else {
-            capacete = true;
-            System.out.println("capacete retraído");
-        }
-        return capacete;
-    }
-    public void atirar(){
-            System.out.println("pew pew pow");
     }
 
-    public void falar(int vfala){
-        System.out.println(falas[vfala-1]);
+    public String retraircapacete() {
+        if (capacete == true) {
+        capacete=false;
+        return ("retraído");
+        } else {
+        capacete=true;
+        return ("aberto");
+        }
+    }
+
+    public String atirar() {
+        return ("pew pew pew pow pow pow");
+    }
+
+    public String falar(int i) {
+        return falas[i];
+    }
+
+    public String golpeando() {
+        return ("golpeando");
     }
 }
