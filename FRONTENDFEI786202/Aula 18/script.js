f8fe946b4da3510e0c86d137b3f74345283e1094
Gustavo Espindola Oselame <143852278+gustavoespindolaoselame@users.noreply.github.com>
2024-04-23 21:27:45 -0300

    commit

---
+++ FRONTENDFEI786202/Aula 18/script.js
@@ -0,0 +1,11 @@
+let i =0;
+while(true){
+    
+    for(i=0;i<255;i++){
+        document.getElementById("banana").style.backgroundColor = i;
+       }
+       for(i=255;i>0;i--){
+        document.getElementById("banana").style.backgroundColor = i;
+       }
+}
+q
